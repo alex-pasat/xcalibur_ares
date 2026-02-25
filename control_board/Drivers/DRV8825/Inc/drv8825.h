ae979461f016abf95ed6ec35924f12b0caba77ed
MGMacD <dmakmakmg@gmail.com>
2026-02-25 04:04:42 -0500

    test driver DRV8825

---
+++ control_board/Drivers/DRV8825/Inc/drv8825.h
@@ -0,0 +1,300 @@
+#ifndef DRV8825_H
+#define DRV8825_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "stm32g4xx.h"
+
+#define DRV8825_NUM_STEPPERS 2
+#define STEPPER_MICROSTEP_ENABLED false
+
+typedef enum
+{
+    DIRECTION_CCW = GPIO_PIN_RESET,
+    DIRECTION_CW = GPIO_PIN_SET
+} drv8825_direction_e;
+
+#if STEPPER_MICROSTEP_ENABLED
+typedef enum
+{
+    MS_FULL = 0,
+    MS_HALF = 1,
+    MS_1_4 = 2,
+    MS_1_8 = 3,
+    MS_1_16 = 4,
+    MS_1_32 = 5 // DRV8825 supports up to 1/32 microstepping
+} microstep_mode_e;
+#endif
+
+#if STEPPER_MICROSTEP_ENABLED
+typedef struct
+{
+    // Step pin. A pulse on this pin will cause the motor to step one increment.
+    // The direction of the step is determined by the state of the direction pin.
+    GPIO_TypeDef *step_port;
+    uint32_t step_pin;
+
+    // Direction pin. Controls the direction of the motor.
+    // High for CW, Low for CCW (or vice versa depending on wiring)
+    GPIO_TypeDef *dir_port;
+    uint32_t dir_pin;
+    bool dir_inverted;
+
+    // absolute current position in steps
+    uint32_t current_pos; // Steps
+
+    // target position in steps
+    uint32_t target_pos; // Steps
+
+    // current speed in steps per second
+    float speed; // Steps per second
+
+    // maximum speed in steps per second
+    // Must be > 0.
+    float max_speed; // Steps per second
+
+    // acceleration for motor. must be > 0.
+    float acceleration; // Steps per second^2
+    float sqrt_twoa;    // Precomputed sqrt(2*acceleration)
+
+    // The last step time in microseconds
+    unsigned long last_step_time;
+
+    // The minimum allowed pulse width in microseconds
+    unsigned int min_pulse_width;
+
+    // Enable pin for stepper driver, or 0xFF if unused.
+    GPIO_TypeDef *en_port;
+    uint32_t en_pin;
+    bool en_inverted;
+
+    // #TODO: Add microstep control pins (M0, M1, M2) if needed for microstepping configuration
+
+    struct
+    {
+        uint32_t n;     // The step counter for speed calculations
+        float c0;       // Initial step size in microseconds
+        float cn;       // Last step size in microseconds
+        float cmin;     // Min step size in microseconds based on maxSpeed
+        bool direction; // Current direction of the motor
+    } state;
+} drv8825_config_t;
+#else
+
+typedef struct
+{
+    // Step pin. A pulse on this pin will cause the motor to step one increment.
+    // The direction of the step is determined by the state of the direction pin.
+    GPIO_TypeDef *step_port;
+    uint32_t step_pin;
+
+    // Direction pin. Controls the direction of the motor.
+    // High for CW, Low for CCW (or vice versa depending on wiring)
+    GPIO_TypeDef *dir_port;
+    uint32_t dir_pin;
+    bool dir_inverted;
+
+    // absolute current position in steps
+    int32_t current_pos; // Steps
+
+    // target position in steps
+    int32_t target_pos; // Steps
+
+    // current speed in steps per second
+    // #TODO: positive is CW?
+    float speed; // Steps per second
+
+    // maximum speed in steps per second
+    // Must be > 0.
+    float max_speed; // Steps per second
+
+    // acceleration for motor. must be > 0.
+    float acceleration; // Steps per second^2
+
+    // The last step time in microseconds
+    uint32_t last_step_time;
+
+    // The minimum allowed pulse width in microseconds
+    uint32_t min_pulse_width;
+
+    // Enable pin for stepper driver, or 0xFF if unused.
+    GPIO_TypeDef *en_port;
+    uint32_t en_pin;
+    bool en_inverted;
+
+    // Fault pin for stepper driver, or 0xFF if unused. Active LOW.
+    GPIO_TypeDef *nfault_port;
+    uint32_t nfault_pin;
+
+    uint32_t n;     // The step counter for speed calculations
+    float c0;       // Initial step size in microseconds
+    float cn;       // Last step size in microseconds
+    float cmin;     // Min step size in microseconds based on maxSpeed
+
+    bool direction; // Current direction of the motor
+    uint32_t step_interval; // Current interval between steps in microseconds
+} drv8825_config_t;
+
+#endif
+
+void DRV8825_Init(
+    drv8825_config_t *config, 
+    GPIO_TypeDef *step_port, 
+    uint32_t step_pin, GPIO_TypeDef 
+    *dir_port, uint32_t dir_pin, 
+    GPIO_TypeDef *en_port, 
+    uint32_t en_pin,
+    GPIO_TypeDef *nfault_port,
+    uint32_t nfault_pin
+);
+
+/**
+ * @brief Set the current position of the motor. This will set both the current
+ * position and the target position to the specified value, and reset the speed
+ * and step interval. This is useful for setting the current position to a known
+ * value, such as after homing the motor.
+ * @param config Pointer to the DRV8825 configuration struct
+ * @param position The current position in steps. Negative is anticlockwise from
+ * the 0 position.
+ */
+void DRV8825_SetCurrentPosition(drv8825_config_t *config, int32_t position);
+
+/**
+ * @brief Sets the desired speed of the motor to use with DRV8825_RunSpeed()
+ * and DRV8825_RunSpeedToPosition().
+ * @param config Pointer to the DRV8825 configuration struct
+ * @param speed The desired speed in steps per second. Positive is clockwise.
+ * Speeds of more than 1000 steps per second are unreliable. Very slow speeds
+ * may be set (eg 0.00027777 for once per hour, approximately). Speed accuracy
+ * depends on the crystal. Jitter depends on how frequently you call the
+ * DRV8825_RunSpeed() function. The speed will be limited by the current value
+ * of max_speed.
+ */
+void DRV8825_SetSpeed(drv8825_config_t *config, float speed);
+
+/**
+ * @brief Sets the maximum permitted speed. The DRV8825_Run() function will
+ * accelerate up to the speed set by this function.
+ * @param config Pointer to the DRV8825 configuration struct
+ * @param max_speed The desired maximum speed in steps per second. Must be > 0
+ * @warning Speeds that exceed the maximum speed supported by the processor may
+ * Result in non-linear accelerations and decelerations.
+ */
+void DRV8825_SetMaxSpeed(drv8825_config_t *config, float max_speed);
+
+/**
+ * @brief Sets the acceleration/deceleration rate.
+ * @param config Pointer to the DRV8825 configuration struct
+ * @param acceleration The desired acceleration in steps per second per second.
+ * Must be > 0.0. This is an expensive call since it requires a square root
+ * to be calculated. Dont call more often than needed.
+ */
+void DRV8825_SetAcceleration(drv8825_config_t *config, float acceleration);
+
+/**
+ * @brief set the target position. The DRV8825_Run() function will try to
+ * move the motor from the current position to the target position, taking
+ * into account the max speed and acceleration.
+ * @attention DRV8825_MoveTo() also recalculates the speed for the next step.
+ * If you are trying to use constant speed movements, you should call
+ * DRV8825_SetSpeed() after calling DRV8825_MoveTo().
+ * @param config Pointer to the DRV8825 configuration struct
+ * @param target_pos The desired absolute position in steps. Negative is
+ * anticlockwise from the 0 position.
+ */
+void DRV8825_MoveTo(drv8825_config_t *config, int32_t target_pos);
+
+/**
+ * @brief Set the target position relative to the current position.
+ * @param config Pointer to the DRV8825 configuration struct
+ * @param relative The desired position relative to the current position.
+ * Negative is anticlockwise from the current position.
+ */
+void DRV8825_Move(drv8825_config_t *config, int32_t relative);
+
+/**
+ * @brief Poll the motor and step it if a step is due, implementing
+ * accelerations and decelerations to achieve the target position.
+ * @attention You must call this as frequently as possible, but at least once
+ * per minimum step time interval, preferably in your main loop. Note that each
+ * call to run() will make at most one step, and then only when a step is due,
+ * based on the current speed and the time since the last step.
+ * @param config Pointer to the DRV8825 configuration struct
+ * @return true if the motor is still running to the target position.
+ */
+bool DRV8825_Run(drv8825_config_t *config);
+
+/**
+ * @brief Poll the motor and step it if a step is due, implementing a constant
+ * speed as set by the most recent call to DRV8825_SetSpeed(). You must call
+ * this as frequently as possible, but at least once per step interval.
+ * @param config Pointer to the DRV8825 configuration struct
+ * @return true if the motor was stepped.
+ */
+bool DRV8825_RunSpeed(drv8825_config_t *config);
+
+/**
+ * @brief Moves the motor (with acceleration/deceleration) to the target
+ * position and blocks until it is at position. 
+ * @warning Don't use this in event loops, since it blocks until the motor is at
+ * position.
+ */
+void DRV8825_RunToPosition(drv8825_config_t *config);
+
+/**
+ * @brief Moves the motor (with acceleration/deceleration) to the new target
+ * position and blocks until it is at position.
+ * @warning Don't use this in event loops, since it blocks until the motor is at
+ * position.
+ * @param config Pointer to the DRV8825 configuration struct
+ * @param position The new target position in steps. Negative is anticlockwise
+ * from the 0 position.
+ * @return true if it stepped
+ */
+bool DRV8825_RunSpeedToPosition(drv8825_config_t *config);
+
+/**
+ * @brief Moves the motor (with acceleration/deceleration) to the new target
+ * position and blocks until it is at position.
+ * @warning Don't use this in event loops, since it blocks until the motor is at
+ * position.
+ * @param config Pointer to the DRV8825 configuration struct
+ * @param position The new target position in steps. Negative is anticlockwise
+ * from the 0 position.
+ */
+void DRV8825_RunToNewPosition(drv8825_config_t *config, int32_t position);
+
+/**
+ * @brief Sets a new target position that causes the stepper to stop as quickly
+ * as possible, using the current speed and acceleration parameters.
+ * @param config Pointer to the DRV8825 configuration struct
+ */
+void DRV8825_Stop(drv8825_config_t *config);
+
+/**
+ * @brief Disable the stepper motor outputs. This will typically disable the
+ * power to the motor coils, saving power. This is useful to support low power
+ * modes: disable the outputs when you dont need to hold position or move the
+ * motor, and enable the outputs when you need to hold position or move the motor.
+ * @param config Pointer to the DRV8825 configuration struct
+ */
+void DRV8825_DisableOutputs(drv8825_config_t *config);
+/**
+ * @brief Enable the stepper motor outputs. This will typically enable the
+ * power to the motor coils. You should call this before trying to hold position
+ * or move the motor, and call DRV8825_DisableOutputs() when you dont need to
+ * hold position or move the motor to save power.
+ * @param config Pointer to the DRV8825 configuration struct
+ */
+void DRV8825_EnableOutputs(drv8825_config_t *config);
+
+/**
+ * @brief Test function to verify that the driver is working. This will step the
+ * motor 100 steps in one direction, then 100 steps in the other direction,
+ * with a delay in between. You can use this to verify that the motor is
+ * connected correctly and that the driver is working.
+ * @param config Pointer to the DRV8825 configuration struct
+ */
+void DRV8825_Test(drv8825_config_t *config);
+
+#endif /* DRV8825_H */
---
+++ control_board/Drivers/DRV8825/Src/drv8825.c
@@ -0,0 +1,281 @@
+#include "drv8825.h"
+
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+
+// Internal helper functions
+
+static uint32_t distTo(drv8825_config_t *config)
+{
+    return config->target_pos - config->current_pos;
+}
+
+static float computeNewSpeed(drv8825_config_t *config)
+{
+    uint32_t dist_to = distTo(config);
+    uint32_t steps_to_stop = (uint32_t)((config->speed * config->speed) / (2.0f * config->acceleration));
+
+    if (dist_to == 0 && steps_to_stop <= 1)
+    {
+        // We are at the target and it's time to stop
+        config->speed = 0.0f;
+        config->n = 0;
+        return 0;
+    }
+
+    if (dist_to > 0)
+    {
+        // We are anticlockwise from the target
+        // Need to go clockwise from here, maybe decelerate now
+        if (config->n > 0)
+        {
+            // Currently accelerating, need to decel now? Or maybe going the wrong way?
+            if ((steps_to_stop >= dist_to) || config->direction == DIRECTION_CCW)
+                config->n = -steps_to_stop; // Start deceleration
+        }
+        else if (config->n < 0)
+        {
+            // Currently decelerating, need to accel again?
+            if ((steps_to_stop < dist_to) && config->direction == DIRECTION_CW)
+                config->n = -config->n; // Start accceleration
+        }
+    }
+    else if (dist_to < 0)
+    {
+        // We are clockwise from the target
+        // Need to go anticlockwise from here, maybe decelerate
+        if (config->n > 0)
+        {
+            // Currently accelerating, need to decel now? Or maybe going the wrong way?
+            if ((steps_to_stop >= -dist_to) || config->direction == DIRECTION_CW)
+                config->n = -steps_to_stop; // Start deceleration
+        }
+        else if (config->n < 0)
+        {
+            // Currently decelerating, need to accel again?
+            if ((steps_to_stop < -dist_to) && config->direction == DIRECTION_CCW)
+                config->n = -config->n; // Start accceleration
+        }
+    }
+
+    if (config->n == 0) {
+        // first step from stopped
+        config->cn = config->c0;
+        config->direction = (dist_to > 0) ? DIRECTION_CW : DIRECTION_CCW;
+    } else {
+        // subsequent step, accelerate or decelerate
+        config->cn = config->cn - ((2.0f * config->cn) / ((4.0f * config->n) + 1));
+        config->cn = (config->cn < config->cmin) ? config->cmin : config->cn;
+    }
+
+    config->n++;
+    config->step_interval = (uint32_t)config->cn;
+    config->speed = 1000000.0f / config->cn;
+    config->speed = (config->direction == DIRECTION_CW) ? config->speed : -config->speed;
+
+    return config->step_interval;
+}
+
+// Public API functions
+
+void DRV8825_Test(drv8825_config_t *config)
+{
+    // Step 100 steps in one direction
+    DRV8825_Move(config, 100);
+    while (DRV8825_Run(config)); // Wait until movement is complete
+
+    // Delay for a bit
+    HAL_Delay(1000);
+
+    // Step 100 steps in the other direction
+    DRV8825_Move(config, -200);
+    while (DRV8825_Run(config)); // Wait until movement is complete{
+    
+}
+
+void DRV8825_Init(drv8825_config_t *config,
+                  GPIO_TypeDef *step_port, uint32_t step_pin,
+                  GPIO_TypeDef *dir_port, uint32_t dir_pin,
+                  GPIO_TypeDef *en_port, uint32_t en_pin,
+                  GPIO_TypeDef *nfault_port,
+                  uint32_t nfault_pin)
+{
+
+    config->step_port = step_port;
+    config->step_pin = step_pin;
+    config->dir_port = dir_port;
+    config->dir_pin = dir_pin;
+    config->en_port = en_port;
+    config->en_pin = en_pin;
+    config->nfault_port = nfault_port;
+    config->nfault_pin = nfault_pin;
+
+    // Set default values
+    config->current_pos = 0;
+    config->target_pos = 0;
+    config->speed = 0.0f;
+    config->max_speed = 0;
+    config->acceleration = 0;
+    config->last_step_time = 0;
+    config->min_pulse_width = 1; // Minimum pulse width in microseconds
+    config->dir_inverted = false;
+    config->en_inverted = false;
+
+    // Initialize GPIO pins
+    HAL_GPIO_WritePin(config->en_port, config->en_pin, GPIO_PIN_SET); // Disable motor by default
+    HAL_GPIO_WritePin(config->dir_port, config->dir_pin, DIRECTION_CW); // Default direction
+    HAL_GPIO_WritePin(config->step_port, config->step_pin, GPIO_PIN_RESET); // Ensure step pin is low
+    if (config->nfault_port != NULL)
+        HAL_GPIO_WritePin(config->nfault_port, config->nfault_pin, GPIO_PIN_RESET); // Clear fault pin if used
+
+    DRV8825_SetAcceleration(config, 1.0f); // Set some reasonable default acceleration
+    DRV8825_SetMaxSpeed(config, 1000.0f); // Set some reasonable default max speed
+}
+
+void DRV8825_SetCurrentPosition(drv8825_config_t *config, int32_t position)
+{
+    config->current_pos = position;
+    config->target_pos = position;
+    config->n = 0;
+    config->step_interval = 0;
+    config->speed = 0.0f;
+}
+
+void DRV8825_SetSpeed(drv8825_config_t *config, float speed)
+{
+    if (speed == config->speed)
+        return;
+    speed = (speed > config->max_speed) ? config->max_speed : ((speed < -config->max_speed) ? -config->max_speed : speed);
+    if (speed == 0.0f)
+        config->step_interval = 0;
+    else
+    {
+        config->step_interval = (uint32_t)fabsf(1000000.0f / speed);
+        config->direction = (speed > 0.0f) ? DIRECTION_CW : DIRECTION_CCW;
+    }
+    config->speed = speed;
+}
+
+void DRV8825_SetMaxSpeed(drv8825_config_t *config, float max_speed)
+{
+    if (max_speed <= 0.0f)
+        return; // Invalid max speed, ignore
+    else if (config->max_speed != max_speed)
+    {
+        config->max_speed = max_speed;
+        config->cmin = 1000000.0f / max_speed;
+        // recompute n from current speed and adjust speed if accelerating or cruising
+        if (config->n > 0)
+        {
+            config->n = (uint32_t)((config->speed * config->speed) / (2.0f * config->acceleration)); // Equation 16
+            computeNewSpeed(config);
+        }
+    }
+
+}
+
+void DRV8825_SetAcceleration(drv8825_config_t *config, float acceleration)
+{
+    if (acceleration <= 0.0f)
+        return; // Invalid acceleration, ignore
+    else if (config->acceleration != acceleration)
+    {
+        config->n = config->n * (config->acceleration / acceleration);
+        config->c0 = 0.676f * sqrtf(2.0f / acceleration) * 1000000.0f; // Equation 15
+        config->acceleration = acceleration;
+        computeNewSpeed(config);
+    }
+}
+
+void DRV8825_MoveTo(drv8825_config_t *config, int32_t target_pos)
+{
+    if (target_pos != config->target_pos) {
+        config->target_pos = target_pos;
+        computeNewSpeed(config);
+    }
+}
+
+void DRV8825_Move(drv8825_config_t *config, int32_t relative)
+{
+    DRV8825_MoveTo(config, config->current_pos + relative);
+}
+
+bool DRV8825_Run(drv8825_config_t *config)
+{
+    if (DRV8825_RunSpeed(config))
+        computeNewSpeed(config);
+    return (config->speed != 0.0f) || distTo(config) != 0;
+}
+
+bool DRV8825_RunSpeed(drv8825_config_t *config)
+{
+    if (config->step_interval == 0)
+        return false; // No movement required
+
+    uint32_t now = HAL_GetTick() * 1000; // Get current time in microseconds
+    uint32_t time_since_last_step = now - config->last_step_time;
+    if (time_since_last_step < config->step_interval)
+        return false; // Not time for the next step yet
+
+    // Time to step
+    if (config->direction == DIRECTION_CCW)
+        config->current_pos++;
+    else
+        config->current_pos--;
+
+    // Pulse the step pin
+    HAL_GPIO_WritePin(config->step_port, config->step_pin, GPIO_PIN_SET);
+    HAL_Delay(config->min_pulse_width / 1000); // Delay for minimum pulse width
+    HAL_GPIO_WritePin(config->step_port, config->step_pin, GPIO_PIN_RESET);
+
+    config->last_step_time = now;
+    return true;
+}
+
+void DRV8825_RunToPosition(drv8825_config_t *config)
+{
+    while (DRV8825_Run(config));
+}
+
+bool DRV8825_RunSpeedToPosition(drv8825_config_t *config)
+{
+    if (config->target_pos == config->current_pos)
+        return false; // Already at position
+    if (config->target_pos > config->current_pos)
+        config->direction = DIRECTION_CCW;
+    else
+        config->direction = DIRECTION_CCW;
+    return DRV8825_RunSpeed(config);
+}
+
+void DRV8825_RunToNewPosition(drv8825_config_t *config, int32_t position)
+{
+    DRV8825_MoveTo(config, position);
+    DRV8825_RunToPosition(config);
+}
+
+void DRV8825_Stop(drv8825_config_t *config)
+{
+    if (config->speed != 0.0f)
+    {
+        uint32_t steps_to_stop = (uint32_t)((config->speed * config->speed) / (2.0f * config->acceleration)) + 1;
+        if (config->speed > 0)
+            DRV8825_Move(config, steps_to_stop);
+        else
+            DRV8825_Move(config, -steps_to_stop);
+    }
+}
+
+void DRV8825_DisableOutputs(drv8825_config_t *config)
+{
+    if (config->en_port != NULL)
+        HAL_GPIO_WritePin(config->en_port, config->en_pin, config->en_inverted ? GPIO_PIN_RESET : GPIO_PIN_SET);
+}
+
+void DRV8825_EnableOutputs(drv8825_config_t *config)
+{
+    if (config->en_port != NULL)
+        HAL_GPIO_WritePin(config->en_port, config->en_pin, config->en_inverted ? GPIO_PIN_SET : GPIO_PIN_RESET);
+}
